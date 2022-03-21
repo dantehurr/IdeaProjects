d51e5acf152ae50e0a04d5f2b7d43f909471f23f
dantehurr <75588090+dantehurr@users.noreply.github.com>
2022-03-20 21:13:54 -0400

    Updated Project4

--- Let'sSortItOut/src/Course.java
+++ Let'sSortItOut/src/Course.java
@@ -1,2 +1,34 @@
-public class Course {
+/*
+* Create a class called Course which stores a department, course number and course name.
+For instance “CS”, 241, “Data Structures and Algorithms”.
+* This class must implement comparable to compare it to another Course
+so they can be sorted: CS 121 would go before CS 241, however CIS 151 would go before CS 121 because CIS
+is earlier in the alphabet than CS. MAT 100 would come after CS 241 because MAT is later in the alphabet than CS.
+* This class should also include a ToString method which will return a String in this
+example format: CS 241: Data Structures and Algorithms.
+ */
+
+
+public class Course implements Comparable<Course>{
+
+    String type, name;
+    int num;
+
+    Course(String course, int number, String title){
+
+        this.type = course;
+        this.num = number;
+        this.name = title;
+    }
+
+    public int compareTo(Course aCourse){
+
+        return Integer.compare(this.num, aCourse.num);
+    }
+
+    public String toString(){
+
+        return;
+    }
+
 }
--- Let'sSortItOut/src/Main.java
+++ Let'sSortItOut/src/Main.java
@@ -1,2 +1,51 @@
+
+/*
+  Your Name:  Dante Hurr
+  Pace Email: dh51481n@pace.edu
+
+  Bubble Sort:
+  What is the worst case big O and what kind of input would cause that?
+  What is the best case big O and what kind of input would cause that?
+
+  Insertion Sort:
+  What is the worst case big O and what kind of input would cause that?
+  What is the best case big O and what kind of input would cause that?
+
+  Selection Sort:
+  What is the worst case big O and what kind of input would cause that?
+  What is the best case big O and what kind of input would cause that?
+*/
+
+import java.util.ArrayList;
+
 public class Main {
+
+    public static void print(ArrayList<Course> a){
+/*
+Inside of Main.java, write a method called print that takes an ArrayList<Course> and prints the list.
+This will come in handy in your testing.
+For instance, you can test if your comparable works by using Collections.sort and then printing the result.
+ */
+    }
+    public static void bubbleSort(ArrayList<Course> a){
+/*
+Write a method called bubbleSort that takes an ArrayList<Course> and performs the bubble sort.
+You may refer to the book, your notes or this Wikipedia article: https://en.wikipedia.org/wiki/Bubble_sort
+ */
+    }
+    public static void insertionSort(ArrayList<Course> a){
+/*
+Write a method called insertionSort that takes an ArrayList<Course> and performs the insertion sort. You may refer to the book, your notes or this Wikipedia article: https://en.wikipedia.org/wiki/Insertion_sort
+ */
+    }
+    public static void selectionSort(ArrayList<Course> a){
+/*
+Write a method called selectionSort that takes an ArrayList<Course> and performs the selection sort. You may refer to the book, your notes or this Wikipedia article: https://en.wikipedia.org/wiki/Selection_sort
+ */
+    }
+
+    public static void main(String[] args) {
+
+
+    }
 }
